0afefeca4b0d1f51e1113b9e2c24cfde28f8200a
“Azmi <Azmitammam@gmail.com>
2022-02-03 04:12:56 +0200

    Add Projects Component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import './Projects.css'
+import { AiOutlineLink } from "react-icons/ai";
 
 function Projects() {
       return (
-            <>
-            </>
+            <div className="projects">
+                  <div className="containerProjects">
+                        <div className="projectCard">
+                              <div className="img">
+                              <img src="https://trio.dev/static/46a74b0f7c9b47353ea207a29731bc51/263a75529a1752b75d64f9f21fd07c92.jpg" alt="project" />
+                              </div>
+                              <div className="info">
+                              <p className='title'>Portfolio Project</p>
+                              <p className='description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, quis?</p>
+                              <div className="tags">
+                              <ul>
+                                    <li>React</li>
+                                    <li>JavaScript</li>
+                                    <li>CSS</li>
+                              </ul>
+                              </div>
+                              </div>
+                              <div className="buttons">
+                              <div className="socialLinks">
+                              <ul>
+                              <a href="/" target="_blank" >
+                              <li><span><AiOutlineLink /></span> Live Demo</li>
+                              </a>
+                              </ul>
+                              </div>
+                              </div>
+                        </div>
+                  </div>
+            </div>
       )
 }
 
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,12 +2,14 @@ import react from "react";
 import Experience from "../Components/Experience/Experience";
 import Header from "../Components/Header/Header";
 import Navbar from "../Components/Navbar/Navbar";
+import Projects from "../Components/Projects/Projects";
 function HomePage() {
       return(
             <>
             <Navbar />
             <Header />
             <Experience />
+            <Projects />
             </>
       );
 }
