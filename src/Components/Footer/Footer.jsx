25de398ae553c18adb66df6f44651ae8c86bebe3
“Azmi <Azmitammam@gmail.com>
2022-02-04 04:11:20 +0200

    Fix Responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
       let year = new Date()
       return(
             <footer>
-                  <div className="topFooter">
-                  </div>
+{/*                   <div className="topFooter">
+                  </div> */}
                   <div className="copyright">
                   <p>Copyright © 2021 - {year.getFullYear()} <a href="https://twitter.com/azzmmii" target="_blank">Azmi Al-Tammam</a>, AT All rights reserved.</p>
                   </div>
