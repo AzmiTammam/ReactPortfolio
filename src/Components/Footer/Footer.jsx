3b70c5426da5b99adaa2255d3f7bdd751fc5b931
“Azmi <Azmitammam@gmail.com>
2022-02-04 06:47:33 +0200

    noreferrer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
 {/*                   <div className="topFooter">
                   </div> */}
                   <div className="copyright">
-                  <p>Copyright © 2021 - {year.getFullYear()} <a href="https://twitter.com/azzmmii" target="_blank">Azmi Al-Tammam</a>, AT All rights reserved.</p>
+                  <p>Copyright © 2021 - {year.getFullYear()} <a href="https://twitter.com/azzmmii" target="_blank" rel="noreferrer">Azmi Al-Tammam</a>, AT All rights reserved.</p>
                   </div>
             </footer>
       )
