0eba7111578e81e0cbb1d38ade9c172032623f69
“Azmi <Azmitammam@gmail.com>
2022-02-06 13:11:12 +0200

    Remove Smoth

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,14 +11,14 @@ function Navbar() {
             <nav>
                   <div className="headTitle">
                   <h3>
-                        Azmi Portfolio
+                        <Link to="/">Azmi Portfolio</Link>
                   </h3>
                   </div>
                   <div className="navLinks">
                   <ul>
-                        <li><Link to="/#projects" spy={true} smooth={true} offset={50} duration={500}>Projects</Link></li>
-                        <li><Link to="/#header" spy={true} smooth={true} offset={50} duration={500}>Resume</Link></li>
-                        <li><Link to="/#experience" spy={true} smooth={true} offset={50} duration={500}>Experience</Link></li>
+                        <li><Link to="/#projects" >Projects</Link></li>
+                        <li><Link to="/#header" >Resume</Link></li>
+                        <li><Link to="/#experience" >Experience</Link></li>
                         <li><Link to="/login">Login</Link></li>
                         <li><MdOutlineDarkMode /></li>
                   </ul>
