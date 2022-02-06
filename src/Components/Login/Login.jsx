473f2e4c99ed141d01b25c92e5163404a5d705f3
“Azmi <Azmitammam@gmail.com>
2022-02-06 09:14:01 +0200

    Add Login Form

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Login.css';
+import { ImWarning } from "react-icons/im";
 
 function Login() {
       return(
             <div className="login">
-                  Login
+                  <h2>Sign in</h2>
+                  <div className="loginContainer">
+                        <form action="login" method="post">
+                              <div className="username">
+                                    <input type="text" placeholder='Username' name='username' required />
+                              </div>
+                              <div className="password">
+                                    <input type="password" placeholder='Password' name='password' required />
+                              </div>
+                              <button type='submit'>Sign in</button>
+                              <span> <ImWarning /> <br />Sign in feature only for <a href="http://twitter.com/azzmmii" target="_blank" rel="noopener noreferrer">Azmi.</a></span>
+                        </form>
+                  </div>
             </div>
       )
 }
