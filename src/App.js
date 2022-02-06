a13046a3218076c3bc1e24b04aa949dc4cedb402
“Azmi <Azmitammam@gmail.com>
2022-02-06 08:37:53 +0200

    Add Login Component and Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import react from "react";
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import './Styles/style.css'
 import HomePage from "./Pages/HomePage";
+import Login from "./Components/Login/Login";
+import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="login" element={<Login />} />
     </Routes>
     </BrowserRouter>
   );
