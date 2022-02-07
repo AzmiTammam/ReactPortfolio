dd1a9316790831947d3aa77c3a30cf181a3780e9
“Azmi <Azmitammam@gmail.com>
2022-02-07 07:26:31 +0200

    Make The Nav Responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,24 +3,30 @@ import react from "react";
 import { Link } from "react-router-dom";
 import './Navbar.css';
 import { MdOutlineDarkMode } from "react-icons/md";
+import { FaBars } from "react-icons/fa";
 
 function Navbar() {
-
-
+      const showMobileNav = () => {
+            let elements = document.querySelectorAll('.navLinks ul li')
+            for (let i = 0; i < elements.length; i++) {
+                  elements[i].classList.toggle('hideLi');
+            }
+      }
       return(
             <nav>
-                  <div className="headTitle">
+                  <div className="headTitle" id="nav">
                   <h3>
                         <Link to="/">Azmi Portfolio</Link>
                   </h3>
                   </div>
                   <div className="navLinks">
                   <ul>
-                        <li><Link to="/#projects" >Projects</Link></li>
-                        <li><Link to="/#header" >Resume</Link></li>
-                        <li><Link to="/#experience" >Experience</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><MdOutlineDarkMode /></li>
+                        <li className="hum hideLi"><Link to="/#projects" >Projects</Link></li>
+                        <li className="hum hideLi"><Link to="/#header" >Resume</Link></li>
+                        <li className="hum hideLi"><Link to="/#experience" >Experience</Link></li>
+                        <li className="hum hideLi"><Link to="/login">Login</Link></li>
+                        <li className="hum hideLi"><p><MdOutlineDarkMode /></p></li>
+                        <button id="MobileNav" className="mobileNav" onClick={showMobileNav}><FaBars /></button>
                   </ul>
                   </div>
             </nav>
