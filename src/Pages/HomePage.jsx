66e99a54217b9bccbad5d1dbe306aa20904c93c0
“Azmi <Azmitammam@gmail.com>
2022-02-08 15:42:48 +0200

    Add Certificate component in homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,12 +3,14 @@ import Experience from "../Components/Experience/Experience";
 import Header from "../Components/Header/Header";
 import Projects from "../Components/Projects/Projects";
 import Footer from "../Components/Footer/Footer";
+import Certificate from "../Components/Certificate/Certificate";
 function HomePage() {
       return(
             <>
             <Header />
             <Experience />
             <Projects />
+            <Certificate />
             <Footer />
             </>
       );
