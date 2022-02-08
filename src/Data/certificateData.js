1959372927f9ca5ad9604fdc96b4e47df5058968
“Azmi <Azmitammam@gmail.com>
2022-02-08 15:42:32 +0200

    Add Certificate Info

---
+++ src/Data/certificateData.js
@@ -0,0 +1,48 @@
+
+import frontEnd from "../Components/Certificate/data/FRU.pdf"
+import backEnd from "../Components/Certificate/data/FLU.pdf"
+import frontEndHsoub from "../Components/Certificate/data/FRH.pdf"
+const certificateData = [
+  {
+    id: 1,
+    title: "Front End Development",
+    description:
+      "Build high quality websites and dynamic applications to create stunning user experiences for the web.",
+    img: "https://res.cloudinary.com/durpirilj/image/upload/v1644321898/imgPortfolioNew/21772542-f3e85ef99f7f4a2af17bf81456ed08e6_bfxsuj.png",
+    imgAlt: "Front-End Development Certificate Image ",
+    repoLink: frontEnd,
+    companyLogo: "https://www.udacity.com/images/svgs/udacity-tt-logo.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Development",
+    description:
+      "Design and develop powerful modern web applications that form the foundation for the apps, websites, and systems.",
+    img: "https://res.cloudinary.com/durpirilj/image/upload/v1644321918/imgPortfolioNew/21772444-f6587047e404e878d5c4de69dfe3681a_q7jr5d.png",
+    imgAlt: "Front-End Development Certificate Image ",
+    repoLink: backEnd,
+    companyLogo: "https://www.udacity.com/images/svgs/udacity-tt-logo.svg",
+  },
+  {
+    id: 3,
+    title: "Front-End Web Development",
+    description:
+      "Build high quality websites and dynamic applications to create stunning user experiences for the web.",
+    img: "https://res.cloudinary.com/durpirilj/image/upload/v1644321915/imgPortfolioNew/21772477-b83c42e8742617d6002933a2bf0180c6_aq2xyr.png",
+    imgAlt: "Front-End Development Certificate Image ",
+    repoLink: frontEndHsoub,
+    companyLogo: "https://static.hsoubcdn.com/assets/images/logos/Hsoub-h200-en-b.png",
+  },
+  {
+    id: 4,
+    title: "Responsive Web Design",
+    description:
+      "Responsive and Modern Web Design by Building 20 Projects, modern Website Code (300 hours)",
+    img: "https://res.cloudinary.com/durpirilj/image/upload/v1644321919/imgPortfolioNew/21771486-eec0d7b2b55828f0bcaa1207afdf0d42_sqytya.png",
+    imgAlt: "Responsive Web Design Certificate Image ",
+    repoLink: "https://www.freecodecamp.org/certification/azmitammam/responsive-web-design",
+    companyLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/FreeCodeCamp_logo.svg/2560px-FreeCodeCamp_logo.svg.png",
+  },
+];
+
+export default certificateData;
